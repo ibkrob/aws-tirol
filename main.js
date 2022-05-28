3f0fddbff20fde1b6c0f7da149b7d83a59ac97d0
ibkrob <102308006+ibkrob@users.noreply.github.com>
2022-05-28 09:44:31 +0200

    rainviewer plugin hinzu und angepasst

--- main.js
+++ main.js
@@ -68,6 +68,17 @@ let getColor = function(value, ramp) {
 };
 
 
+// Rainviewer
+L.control.rainviewer({
+    position: 'bottomleft',
+    nextButtonText: '>',
+    playStopButtonText: 'Play/Stop',
+    prevButtonText: '<',
+    positionSliderLabelText: "Hour:",
+    opacitySliderLabelText: "Opacity:",
+    animationInterval: 500,
+    opacity: 0.5
+}).addTo(map);
 
 
 
